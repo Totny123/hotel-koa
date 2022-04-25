4e596744dd73da9a166161c59b9613a843c900d7
liujunfeng <24455370@qq.com>
2022-04-25 18:18:32 +0800

    feat: init

---
+++ src/app/error-handle.js
@@ -0,0 +1,20 @@
+const errorTypes = require("../constants/error-types");
+
+const errorHandler = (error, ctx) => {
+  let status, message;
+
+  switch (error.message) {
+    case errorTypes.NAME_OR_PASSWORD_IS_REQUIRED:
+      status = 400; // Bad Request
+      message = "用户名或者密码不能为空~";
+      break;
+    default:
+      status = 404;
+      message = "NOT FOUND";
+  }
+
+  ctx.status = status;
+  ctx.body = message;
+};
+
+module.exports = errorHandler;
